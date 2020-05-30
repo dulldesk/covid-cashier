fdf9b2105659a26a5de1d6209f172a88c37ed25c
dulldesk <54601393+dulldesk@users.noreply.github.com>
2020-05-29 22:29:22 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -77,7 +77,7 @@ public class Player extends Character {
 	private void loadMovement() {
 		movement.put("player-up", new Movement(KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0), new AbstractAction() {
 		    public void actionPerformed(ActionEvent e) {
-		    	direction = 'N';
+		    	direction = 'n';
 		    	y_coord--;
 		    	CovidCashier.frame.repaint();
 		    }
@@ -85,7 +85,7 @@ public class Player extends Character {
 
 		movement.put("player-down", new Movement(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0),new AbstractAction() {
 		    public void actionPerformed(ActionEvent e) {
-		    	direction = 'S';
+		    	direction = 's';
 		    	y_coord++;
 		    	CovidCashier.frame.repaint();
 		    }
@@ -93,7 +93,7 @@ public class Player extends Character {
 
 		movement.put("player-left", new Movement(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0), new AbstractAction() {
 		    public void actionPerformed(ActionEvent e) {
-		    	direction = 'W';
+		    	direction = 'w';
 		    	x_coord--;
 		    	CovidCashier.frame.repaint();
 		    }
@@ -101,7 +101,7 @@ public class Player extends Character {
 
 		movement.put("player-right", new Movement(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0), new AbstractAction() {
 		    public void actionPerformed(ActionEvent e) {
-		    	direction = 'E';
+		    	direction = 'e';
 		    	x_coord++;
 		    	CovidCashier.frame.repaint();
 		    }
