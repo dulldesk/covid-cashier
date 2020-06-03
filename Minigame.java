464d6f86fd7cb0254b0489b16e79965ef089b32a
rcshim04 <rcshim04@gmail.com>
2020-06-03 17:16:48 -0400

    Create Minigame.java

---
+++ Minigame.java
@@ -0,0 +1,49 @@
+/**
+  * A minigame
+  * 
+  * Last edit: 6/3/2020
+  * @author 	Eric
+  * @version 	1.0
+  * @since 		1.0
+  */
+
+import java.awt.*;
+import javax.swing.*;
+
+public abstract class Minigame {
+    /**
+	  * The score of the game
+	  */
+    protected double score = 0;
+
+    /**
+      * Increase the score by an amount
+      * @param add      
+	  */
+    protected void add(double add) {
+        score += add;
+    }
+
+    /**
+      * Set the score to an amount
+      * @param set      
+	  */
+      protected void add(double set) {
+        score = set;
+    }
+
+    /**
+     * Reset the score to zero
+     */
+    protected void clear() {
+        score = 0;
+    }
+
+    /**
+     * Get the current score
+     * @return score
+     */
+    protected double getScore() {
+        return score;
+    }
+}
