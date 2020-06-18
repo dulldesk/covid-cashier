bd5b97f8cdbab98c8c9b418a3a80f934f6a92a04
dulldesk <54601393+dulldesk@users.noreply.github.com>
2020-06-18 18:33:26 -0400

    Update User.java

--- User.java
+++ User.java
@@ -1,7 +1,7 @@
 /**
   * User data of the current session
   * 
-  * Last edit: 5/29/2020
+  * Last edit: 6/18/2020
   * @author 	Celeste, Eric
   * @version 	1.0
   * @since 		1.0
@@ -17,4 +17,22 @@ public final class User {
 	  * The user's selected name
 	  */
 	public static String name;
+
+	/**
+	  * Whether the user has trained
+	  */
+	public static boolean hasTrained;
+
+	/**
+	  * Whether it is the first time that the user entered the main menu 
+	  */
+	public static boolean firstMainMenu;
+
+	static {
+		gender = 'M';
+		name = "fellow";
+		hasTrained = false;
+		firstMainMenu = true;
+	}
+
 }
